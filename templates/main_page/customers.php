cd3b714aa6b11548a1995c738ad4338da597fbb7
gandomi <m.gandomi1995@gmail.com>
2023-10-27 20:43:59 +0330

    add video section

--- templates/main_page/customers.php
+++ templates/main_page/customers.php
@@ -8,141 +8,51 @@
             <div class="col d-flex align-items-center">
 
                 <!-- Section Title Icon -->
-                <div class="section-title__icon ms-4">
-                    <span class="icon-briefcase-bulk text-primary">
-                        <span class="path1"></span>
-                        <span class="path2"></span>
-                    </span>
+                <div class="section-title__icon ms-4 text-primary">
+                    <?php echo get_field('customers_icon'); ?>
                 </div>
 
                 <!-- Section Title Text -->
                 <div class="section-title__text">
-                    <p class="font-pinar fw-500 fs-2 mb-2 text-gray-200">مشتریان</p>
-                    <h2 class="display-4 fw-800 text-gray-100">کسب و کارها</h2>
+                    <p class="font-pinar fw-500 fs-2 mb-2 text-gray-200"><?php echo get_field('customers_subtitle'); ?></p>
+                    <h2 class="display-4 fw-800 text-gray-100"><?php echo get_field('customers_title'); ?></h2>
                 </div>
             </div>
         </div>
 
         <div class="row">
-            <!-- Customer Cart -->
-            <div class="col-12 mb-5 mb-xxl-0 col-md-6 col-xxl-3">
-                <div class="customers__card p-7 bg-gray-800 rounded-6">
 
-                    <!-- Customer Cart Ttile -->
-                    <div class="customers__card__title d-flex align-items-center mb-6">
-                        <div class="customers__card__icon-wrapper bg-gray-700 d-flex align-items-center justify-content-center rounded-2 ms-3">
-                            <span class="display-1 icon-car-bulk text-primary">
-                                <span class="path1"></span>
-                                <span class="path2"></span>
-                                <span class="path3"></span>
-                                <span class="path4"></span>
-                            </span>
-                        </div>
-                        <div>
-                            <h3 class="display-5 fw-800 text-gray-100">خدمات خودرو</h3>
-                            <span class="font-pinar text-gray-200">۲۰۰ مشتری</span>
-                        </div>
-                    </div>
 
-                    <!-- Customer Cart's Customers -->
-                    <h4 class="fs-3 text-white-50 fw-600 mb-6">برخی از مشتریان <span class="text-secondary">خدمات خودرو</span></h4>
+            <?php if( have_rows('customers_content') ): ?>
+                <?php while( have_rows('customers_content') ): the_row(); ?>
+                    <!-- Customer Cart -->
+                    <div class="col-12 mb-5 mb-xxl-0 col-md-6 col-xxl-3">
+                        <div class="customers__card p-7 bg-gray-800 rounded-6">
 
-                    <div class="customers__card__tags">
-                        <button class="font-pinar fs-3 btn btn-gray-700 text-white-500 p-2 ms-1 mb-2 rounded-3">کیا موتور</button>
-                        <button class="font-pinar fs-3 btn btn-gray-700 text-white-500 p-2 ms-1 mb-2 rounded-3">مدیران خودرو</button>
-                        <button class="font-pinar fs-3 btn btn-gray-700 text-white-500 p-2 ms-1 mb-2 rounded-3">ایران خودرو</button>
-                    </div>
-                </div>
-            </div>
-            <!-- Customer Cart -->
-            <div class="col-12 mb-5 mb-xxl-0 col-md-6 col-xxl-3">
-                <div class="customers__card p-7 bg-gray-800 rounded-6">
+                            <!-- Customer Cart Ttile -->
+                            <div class="customers__card__title d-flex align-items-center mb-6">
+                                <div class="customers__card__icon-wrapper bg-gray-700 d-flex align-items-center justify-content-center rounded-2 ms-3 <?php echo get_sub_field("icon_color"); ?>">
+                                    <?php echo get_sub_field('icon'); ?>
+                                </div>
+                                <div>
+                                    <h3 class="display-5 fw-800 text-gray-100"><?php echo get_sub_field('title'); ?></h3>
+                                    <span class="font-pinar text-gray-200"><?php echo get_sub_field('subtitle'); ?></span>
+                                </div>
+                            </div>
 
-                    <!-- Customer Cart Ttile -->
-                    <div class="customers__card__title d-flex align-items-center mb-6">
-                        <div class="customers__card__icon-wrapper bg-gray-700 d-flex align-items-center justify-content-center rounded-2 ms-3">
-                            <span class="display-1 icon-brush-bulk text-danger">
-                                <span class="path1"></span>
-                                <span class="path2"></span>
-                                <span class="path3"></span>
-                            </span>
-                        </div>
-                        <div>
-                            <h3 class="display-5 fw-800 text-gray-100">سالن زیبایی</h3>
-                            <span class="font-pinar text-gray-200">۲۰۰ مشتری</span>
+                            <!-- Customer Cart's Customers -->
+                            <h4 class="fs-3 text-white-50 fw-600 mb-6"><?php echo get_sub_field('text'); ?></h4>
+                            <div class="customers__card__tags">
+                                <?php if( have_rows('customers_customers') ): ?>
+                                    <?php while( have_rows('customers_customers') ): the_row(); ?>
+                                        <button class="font-pinar fs-3 btn btn-gray-700 text-white-500 p-2 ms-1 mb-2 rounded-3"><?php echo get_sub_field('title'); ?></button>
+                                    <?php endwhile; ?>
+                                <?php endif; ?>
+                            </div>
                         </div>
                     </div>
-
-                    <!-- Customer Cart's Customers -->
-                    <h4 class="fs-3 text-white-50 fw-600 mb-6">برخی از مشتریان <span class="text-secondary">سالن زیبایی</span></h4>
-
-                    <div class="customers__card__tags">
-                        <button class="font-pinar fs-3 btn btn-gray-700 text-white-500 p-2 ms-1 mb-2 rounded-3">کیا موتور</button>
-                        <button class="font-pinar fs-3 btn btn-gray-700 text-white-500 p-2 ms-1 mb-2 rounded-3">مدیران خودرو</button>
-                        <button class="font-pinar fs-3 btn btn-gray-700 text-white-500 p-2 ms-1 mb-2 rounded-3">ایران خودرو</button>
-                    </div>
-                </div>
-            </div>
-            <!-- Customer Cart -->
-            <div class="col-12 mb-5 mb-xxl-0 col-md-6 col-xxl-3">
-                <div class="customers__card p-7 bg-gray-800 rounded-6">
-
-                    <!-- Customer Cart Ttile -->
-                    <div class="customers__card__title d-flex align-items-center mb-6">
-                        <div class="customers__card__icon-wrapper bg-gray-700 d-flex align-items-center justify-content-center rounded-2 ms-3">
-                            <span class="display-1 icon-lamp-bulk text-secondary">
-                                <span class="path1"></span>
-                                <span class="path2"></span>
-                            </span>
-                        </div>
-                        <div>
-                            <h3 class="display-5 fw-800 text-gray-100">لوازم منزل</h3>
-                            <span class="font-pinar text-gray-200">۲۰۰ مشتری</span>
-                        </div>
-                    </div>
-
-                    <!-- Customer Cart's Customers -->
-                    <h4 class="fs-3 text-white-50 fw-600 mb-6">برخی از مشتریان <span class="text-secondary">لوازم منزل</span></h4>
-
-                    <div class="customers__card__tags">
-                        <button class="font-pinar fs-3 btn btn-gray-700 text-white-500 p-2 ms-1 mb-2 rounded-3">کیا موتور</button>
-                        <button class="font-pinar fs-3 btn btn-gray-700 text-white-500 p-2 ms-1 mb-2 rounded-3">مدیران خودرو</button>
-                        <button class="font-pinar fs-3 btn btn-gray-700 text-white-500 p-2 ms-1 mb-2 rounded-3">ایران خودرو</button>
-                    </div>
-                </div>
-            </div>
-            <!-- Customer Cart -->
-            <div class="col-12 mb-5 mb-xxl-0 col-md-6 col-xxl-3">
-                <div class="customers__card p-7 bg-gray-800 rounded-6">
-
-                    <!-- Customer Cart Ttile -->
-                    <div class="customers__card__title d-flex align-items-center mb-6">
-                        <div class="customers__card__icon-wrapper bg-gray-700 d-flex align-items-center justify-content-center rounded-2 ms-3">
-                            <span class="display-1 icon-coffee-bulk text-warning">
-                                <span class="path1"></span>
-                                <span class="path2"></span>
-                                <span class="path3"></span>
-                                <span class="path4"></span>
-                                <span class="path5"></span>
-                                <span class="path6"></span>
-                            </span>
-                        </div>
-                        <div>
-                            <h3 class="display-5 fw-800 text-gray-100">کافی شاپ</h3>
-                            <span class="font-pinar text-gray-200">۲۰۰ مشتری</span>
-                        </div>
-                    </div>
-
-                    <!-- Customer Cart's Customers -->
-                    <h4 class="fs-3 text-white-50 fw-600 mb-6">برخی از مشتریان <span class="text-secondary">کافی شاپ</span></h4>
-
-                    <div class="customers__card__tags">
-                        <button class="font-pinar fs-3 btn btn-gray-700 text-white-500 p-2 ms-1 mb-2 rounded-3">کیا موتور</button>
-                        <button class="font-pinar fs-3 btn btn-gray-700 text-white-500 p-2 ms-1 mb-2 rounded-3">مدیران خودرو</button>
-                        <button class="font-pinar fs-3 btn btn-gray-700 text-white-500 p-2 ms-1 mb-2 rounded-3">ایران خودرو</button>
-                    </div>
-                </div>
-            </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
         </div>
     </div>
 </section>
