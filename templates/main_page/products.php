ac7c15ec61647a8cc051a0541d305588eb160456
gandomi <m.gandomi1995@gmail.com>
2023-10-27 17:39:12 +0330

    add products section acf template

--- templates/main_page/products.php
+++ templates/main_page/products.php
@@ -8,22 +8,13 @@
 
                 <!-- Section Title Icon -->
                 <div class="section-title__icon ms-4">
-                    <span class="icon-direct-bulk text-primary">
-                        <span class="path1"></span>
-                        <span class="path2"></span>
-                        <span class="path3"></span>
-                        <span class="path4"></span>
-                        <span class="path5"></span>
-                        <span class="path6"></span>
-                        <span class="path7"></span>
-                        <span class="path8"></span>
-                    </span>
+                    <?php echo get_field('products_icon'); ?>
                 </div>
 
                 <!-- Section Title Text -->
                 <div class="section-title__text">
-                    <p class="font-pinar fw-500 fs-2 mb-2 text-gray-200">دایرکت</p>
-                    <h2 class="display-4 fw-800 text-gray-100">محصولات</h2>
+                    <p class="font-pinar fw-500 fs-2 mb-2 text-gray-200"><?php echo get_field('products_subtitle'); ?></p>
+                    <h2 class="display-4 fw-800 text-gray-100"><?php echo get_field('products_title'); ?></h2>
                 </div>
             </div>
         </div>
@@ -31,79 +22,45 @@
         <!-- Products Cards -->
         <div class="row">
 
-            <!-- Product Card -->
-            <div class="col-12 col-xxl-6 mb-8 mb-xxl-0">
-                <div class="direct-card d-flex flex-column flex-md-row bg-gray-800 rounded-6 p-6">
-                    <div class="mx-auto mb-7 mb-md-0 ms-md-9 d-flex align-items-center">
-                        <img class="p-2" src="images/main/product_plus_pro.webp" alt="">
-                    </div>
-                    <div>
-                        <h3 class="fs-2 text-white-50 mb-4"><strong>دایرکت پرو</strong> <span class="text-secondary">( دایرکت پلاس + دستگاه پوز)</span></h3>
-                        <p class="fs-4 mb-7 text-gray-200">دایرکت پرو یک محصول کامل و حرفه‌ای با ویژگی‌های منحصر به فرد است که تمامی نیازهای فروش شما را برآورده می‌کند یک باشگاه مشتریان حرفه ای راه اندازی کنید. در دایرکت پرو کارت عضویت هر مشتری در باشگاه مشتریان همان کارت بانکی اوست.</p>
-                        <ul class="fs-4 mb-7 text-gray-50">
-                            <li class="mb-5 d-flex align-items-center">
-                                <span class="icon-tick-bulk fs-3 text-success ms-3">
-                                    <span class="path1"></span>
-                                    <span class="path2"></span>
-                                </span>
-                                <span>داشبوردی کامل از تمامی تراکنش‌های شما</span>
-                            </li>
-                            <li class="mb-5 d-flex align-items-center">
-                                <span class="icon-tick-bulk fs-3 text-success ms-3">
-                                    <span class="path1"></span>
-                                    <span class="path2"></span>
-                                </span>
-                                <span>مشاهده لیست کامل تراکنش‌های بانکی بدون مراجعه به بانک</span>
-                            </li>
-                            <li class="mb-5 d-flex align-items-center">
-                                <span class="icon-tick-bulk fs-3 text-success ms-3">
-                                    <span class="path1"></span>
-                                    <span class="path2"></span>
-                                </span>
-                                <span>همه ویژگی ها و پشتیبانی پریمیوم را دارد</span>
-                            </li>
-                        </ul>
-                        <button type="button" class="btn btn-primary w-100 text-white-500 p-3 fw-500 rounded-3">جزئیات بیشتر</button>
-                    </div>
-                </div>
-            </div>
+            <?php if( have_rows('products_content') ): ?>
+                <?php while( have_rows('products_content') ): the_row(); ?>
+                    <!-- Product Card -->
+                    <div class="col-12 col-xxl-6 mb-8 mb-xxl-0">
+                        <div class="direct-card d-flex flex-column flex-md-row bg-gray-800 rounded-6 p-6">
+                            <div class="mx-auto mb-7 mb-md-0 ms-md-9 d-flex align-items-center">
+                                <?php $product_image = get_sub_field('image'); ?>
+                                <?php if( !empty( $product_image ) ): ?>
+                                    <img class="p-2" src="<?php echo esc_url($product_image['url']); ?>" alt="<?php echo esc_attr($product_image['alt']); ?>" >
+                                <?php endif; ?>
+                            </div>
+                            <div>
+                                <h3 class="fs-2 text-white-50 mb-4"><?php echo get_sub_field("title"); ?></h3>
+                                <p class="fs-4 mb-7 text-gray-200"><?php echo get_sub_field("text"); ?></p>
+                                <ul class="fs-4 mb-7 text-gray-50">
 
-            <!-- Product Card -->
-            <div class="col-12 col-xxl-6 mb-8 mb-xxl-0">
-                <div class="direct-card d-flex flex-column flex-md-row bg-gray-800 rounded-6 p-6">
-                    <div class="mx-auto mb-7 mb-md-0 ms-md-9 d-flex align-items-center">
-                        <img class="p-2" src="images/main/product_plus.webp" alt="">
+                                <?php if( have_rows('list') ): ?>
+                                    <?php while( have_rows('list') ): the_row(); ?>
+                                        <li class="mb-5 d-flex align-items-center">
+                                            <span class="icon-tick-bulk fs-3 text-success ms-3">
+                                                <span class="path1"></span>
+                                                <span class="path2"></span>
+                                            </span>
+                                            <span><?php echo get_sub_field('list_item'); ?></span>
+                                        </li>
+                                    <?php endwhile; ?>
+                                <?php endif; ?>
+
+
+                                </ul>
+                                <button type="button" class="btn btn-primary w-100 text-white-500 p-3 fw-500 rounded-3">جزئیات بیشتر</button>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <h3 class="fs-2 text-white-50 mb-4"><strong>دایرکت پلاس</strong></h3>
-                        <p class="fs-4 mb-7 text-gray-200">دایرکت پلاس یک دستگاه ذخیره شماره موبایل به همراه ۲۱ امکان جذاب و متنوع است که می‌توانید به کمک آن باشگاه مشتریان‌تان را بیاندازید و با بهترین تکنیک‌‎های فروش آن‌ها را به خرید بیشتر تشویق کنید.</p>
-                        <ul class="fs-4 mb-7 text-gray-50">
-                            <li class="mb-5 d-flex align-items-center">
-                                <span class="icon-tick-bulk fs-3 text-success ms-3">
-                                    <span class="path1"></span>
-                                    <span class="path2"></span>
-                                </span>
-                                <span>ارسال کارت ویزیت دیجیتال و معرفی راه‌های ارتباطی</span>
-                            </li>
-                            <li class="mb-5 d-flex align-items-center">
-                                <span class="icon-tick-bulk fs-3 text-success ms-3">
-                                    <span class="path1"></span>
-                                    <span class="path2"></span>
-                                </span>
-                                <span>راه‌اندازی باشگاه مشتریان، حفظ و وفادارسازی مشتری</span>
-                            </li>
-                            <li class="mb-5 d-flex align-items-center">
-                                <span class="icon-tick-bulk fs-3 text-success ms-3">
-                                    <span class="path1"></span>
-                                    <span class="path2"></span>
-                                </span>
-                                <span>ارسال پیامک گروهی، هدفمند و خودکار</span>
-                            </li>
-                        </ul>
-                        <button type="button" class="btn btn-primary w-100 text-white-500 p-3 fw-500 rounded-3">جزئیات بیشتر</button>
-                    </div>
-                </div>
-            </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+
+
+
 
         </div>
     </div>
