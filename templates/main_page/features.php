4945fc2a0051856dcb014ce341981c6428acd7e6
gandomi <m.gandomi1995@gmail.com>
2023-10-27 19:24:30 +0330

    add features sectoin acf template

--- templates/main_page/features.php
+++ templates/main_page/features.php
@@ -7,18 +7,14 @@
             <div class="col d-flex align-items-center">
 
                 <!-- Section Title Icon -->
-                <div class="section-title__icon ms-4">
-                    <span class="icon-monitor-mobbile-bulk text-warning">
-                        <span class="path1"></span>
-                        <span class="path2"></span>
-                        <span class="path3"></span>
-                    </span>
+                <div class="section-title__icon ms-4 text-warning">
+                    <?php echo get_field('features_icon') ?>
                 </div>
 
                 <!-- Section Title Text -->
                 <div class="section-title__text">
-                    <p class="font-pinar fw-500 fs-2 mb-2 text-gray-200">افزونه</p>
-                    <h2 class="display-4 fw-800 text-gray-100">برترین امکانات</h2>
+                    <p class="font-pinar fw-500 fs-2 mb-2 text-gray-200"><?php echo get_field('features_subtitle') ?></p>
+                    <h2 class="display-4 fw-800 text-gray-100"><?php echo get_field('features_title') ?></h2>
                 </div>
             </div>
         </div>
@@ -31,134 +27,84 @@
 
                     <!-- Features Tabs Btns -->
                     <div class="direct-tabs__btns p-1 bg-gray-800 rounded-3 d-inline-block mb-12">
-                        <button class="fs-2  btn btn-gray-900 text-white-500 px-5 py-3 rounded-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">پنل دایرکت</button>
-                        <button class="fs-2  btn btn-gray-900 text-white-500 px-5 py-3 rounded-3 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">اپلیکیشن</button>
-                        <button class="fs-2  btn btn-gray-900 text-white-500 px-5 py-3 rounded-3 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">فروشگاه ساز</button>
+                        <?php if( have_rows('features_content') ): ?>
+                            <?php $firstLoop = true; ?>
+                            <?php $id = 1; ?>
+                            <?php while( have_rows('features_content') ): the_row(); ?>
+
+                                <?php if ($firstLoop): ?>
+                                    <?php $firstLoop = false; ?>
+                                    <button class="fs-2 btn btn-gray-900 text-white-500 px-5 py-3 rounded-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $id; ?>" aria-expanded="true" aria-controls="collapse<?php echo $id; ?>"><?php echo get_sub_field('tab_name') ?></button>
+                                <?php else: ?>
+                                    <button class="fs-2 btn btn-gray-900 text-white-500 px-5 py-3 rounded-3 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $id; ?>" aria-expanded="false" aria-controls="collapse<?php echo $id; ?>"><?php echo get_sub_field('tab_name') ?></button>
+                                <?php endif; ?>
+
+                                <?php $id++ ?>
+
+                            <?php endwhile; ?>
+                        <?php endif; ?>
                     </div>
 
+                    <!-- Features Tab Content -->
+                    <?php if( have_rows('features_content') ): ?>
+                        <?php $firstLoop = true; ?>
+                        <?php $id = 1; ?>
+                        <?php while( have_rows('features_content') ): the_row(); ?>
 
-                    <div class="direct-tabs__content accordion-item bg-black-500 border-0">
-                        <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-
-                            <!-- Features Tabs Content -->
-                            <div class="direct-tabs__content mb-7 user-select-none">
-                                <div class="form-check form-switch">
-                                    <input class="form-check-input mt-6" type="checkbox" role="switch" id="menuBart" checked>
-                                    <label class="form-check-label text-white-500 me-3" for="menuBart">
-                                        <span class="font-pinar text-gray-100 display-5 d-block fw-800">منو بار پیشرفته</span>
-                                        <span class="text-gray-500">اطلاعات ورود من را برای دفعه بعد ذخیره کنید.</span>
-                                    </label>
+                            <?php if ($firstLoop): ?>
+                                <?php $firstLoop = false; ?>
+                                <div class="direct-tabs__content accordion-item bg-black-500 border-0">
+                                    <div id="collapse<?php echo $id; ?>" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                                        <!-- Features Tabs Content -->
+                                        <?php if( have_rows('tab_content') ): ?>
+                                            <?php while( have_rows('tab_content') ): the_row(); ?>
+                                                <div class="direct-tabs__content mb-7 user-select-none">
+                                                    <div class="form-check form-switch">
+                                                        <input class="form-check-input mt-6" type="checkbox" role="switch" id="orderCheck">
+                                                        <label class="form-check-label text-white-500 me-3" for="orderCheck">
+                                                            <span class="font-pinar text-gray-100 display-5 d-block fw-800"><?php echo get_sub_field('title') ?></span>
+                                                            <span class="text-gray-500"><?php echo get_sub_field('subtitle') ?></span>
+                                                        </label>
+                                                    </div>
+                                                </div>
+                                            <?php endwhile; ?>
+                                        <?php endif; ?>
+                                    </div>
                                 </div>
-                            </div>
-
-                            <!-- Features Tabs Content -->
-                            <div class="direct-tabs__content mb-7 user-select-none">
-                                <div class="form-check form-switch">
-                                    <input class="form-check-input mt-6" type="checkbox" role="switch" id="orderCheck">
-                                    <label class="form-check-label text-white-500 me-3" for="orderCheck">
-                                        <span class="font-pinar text-gray-100 display-5 d-block fw-800">صورتحساب</span>
-                                        <span class="text-gray-500">اطلاعات ورود من را برای دفعه بعد ذخیره کنید.</span>
-                                    </label>
+                            <?php else: ?>
+                                <div class="direct-tabs__content accordion-item bg-black-500 border-0">
+                                    <div id="collapse<?php echo $id; ?>" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                        <!-- Features Tabs Content -->
+                                        <?php if( have_rows('tab_content') ): ?>
+                                            <?php while( have_rows('tab_content') ): the_row(); ?>
+                                                <div class="direct-tabs__content mb-7 user-select-none">
+                                                    <div class="form-check form-switch">
+                                                        <input class="form-check-input mt-6" type="checkbox" role="switch" id="orderCheck">
+                                                        <label class="form-check-label text-white-500 me-3" for="orderCheck">
+                                                            <span class="font-pinar text-gray-100 display-5 d-block fw-800"><?php echo get_sub_field('title') ?></span>
+                                                            <span class="text-gray-500"><?php echo get_sub_field('subtitle') ?></span>
+                                                        </label>
+                                                    </div>
+                                                </div>
+                                            <?php endwhile; ?>
+                                        <?php endif; ?>
+                                    </div>
                                 </div>
-                            </div>
+                            <?php endif; ?>
 
-                            <!-- Features Tabs Content -->
-                            <div class="direct-tabs__content mb-7 user-select-none">
-                                <div class="form-check form-switch">
-                                    <input class="form-check-input mt-6" type="checkbox" role="switch" id="rememberMe">
-                                    <label class="form-check-label text-white-500 me-3" for="rememberMe">
-                                        <span class="font-pinar text-gray-100 display-5 d-block fw-800">مرا به خاطر بسپار</span>
-                                        <span class="text-gray-500">اطلاعات ورود من را برای دفعه بعد ذخیره کنید.</span>
-                                    </label>
-                                </div>
-                            </div>
+                            <?php $id++ ?>
 
-                        </div>
-                    </div>
-
-                    <div class="direct-tabs__content accordion-item bg-black-500 border-0">
-                        <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-
-                            <!-- Features Tabs Content -->
-                            <div class="direct-tabs__content mb-7 user-select-none">
-                                <div class="form-check form-switch">
-                                    <input class="form-check-input mt-6" type="checkbox" role="switch" id="menuBart1" checked>
-                                    <label class="form-check-label text-white-500 me-3" for="menuBart1">
-                                        <span class="font-pinar text-gray-100 display-5 d-block fw-800">منو بار پیشرفته</span>
-                                        <span class="text-gray-500">اطلاعات ورود من را برای دفعه بعد ذخیره کنید.</span>
-                                    </label>
-                                </div>
-                            </div>
-
-                            <!-- Features Tabs Content -->
-                            <div class="direct-tabs__content mb-7 user-select-none">
-                                <div class="form-check form-switch">
-                                    <input class="form-check-input mt-6" type="checkbox" role="switch" id="orderCheck1">
-                                    <label class="form-check-label text-white-500 me-3" for="orderCheck1">
-                                        <span class="font-pinar text-gray-100 display-5 d-block fw-800">صورتحساب</span>
-                                        <span class="text-gray-500">اطلاعات ورود من را برای دفعه بعد ذخیره کنید.</span>
-                                    </label>
-                                </div>
-                            </div>
-
-                            <!-- Features Tabs Content -->
-                            <div class="direct-tabs__content mb-7 user-select-none">
-                                <div class="form-check form-switch">
-                                    <input class="form-check-input mt-6" type="checkbox" role="switch" id="rememberMe1">
-                                    <label class="form-check-label text-white-500 me-3" for="rememberMe1">
-                                        <span class="font-pinar text-gray-100 display-5 d-block fw-800">مرا به خاطر بسپار</span>
-                                        <span class="text-gray-500">اطلاعات ورود من را برای دفعه بعد ذخیره کنید.</span>
-                                    </label>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div class="direct-tabs__content accordion-item bg-black-500 border-0">
-                        <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-
-                            <!-- Features Tabs Content -->
-                            <div class="direct-tabs__content mb-7 user-select-none">
-                                <div class="form-check form-switch">
-                                    <input class="form-check-input mt-6" type="checkbox" role="switch" id="menuBart2" checked>
-                                    <label class="form-check-label text-white-500 me-3" for="menuBart2">
-                                        <span class="font-pinar text-gray-100 display-5 d-block fw-800">منو بار پیشرفته</span>
-                                        <span class="text-gray-500">اطلاعات ورود من را برای دفعه بعد ذخیره کنید.</span>
-                                    </label>
-                                </div>
-                            </div>
-
-                            <!-- Features Tabs Content -->
-                            <div class="direct-tabs__content mb-7 user-select-none">
-                                <div class="form-check form-switch">
-                                    <input class="form-check-input mt-6" type="checkbox" role="switch" id="orderCheck2">
-                                    <label class="form-check-label text-white-500 me-3" for="orderCheck2">
-                                        <span class="font-pinar text-gray-100 display-5 d-block fw-800">صورتحساب</span>
-                                        <span class="text-gray-500">اطلاعات ورود من را برای دفعه بعد ذخیره کنید.</span>
-                                    </label>
-                                </div>
-                            </div>
-
-                            <!-- Features Tabs Content -->
-                            <div class="direct-tabs__content mb-7 user-select-none">
-                                <div class="form-check form-switch">
-                                    <input class="form-check-input mt-6" type="checkbox" role="switch" id="rememberMe2">
-                                    <label class="form-check-label text-white-500 me-3" for="rememberMe2">
-                                        <span class="font-pinar text-gray-100 display-5 d-block fw-800">مرا به خاطر بسپار</span>
-                                        <span class="text-gray-500">اطلاعات ورود من را برای دفعه بعد ذخیره کنید.</span>
-                                    </label>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
 
                 </div>
             </div>
 
             <div class="col-12 col-xl-6">
-                <img class="w-100" src="images/main/features_panel.webp" alt="">
+                <?php $features_image = get_field('features_image'); ?>
+                <?php if( !empty( $features_image ) ): ?>
+                    <img class="w-100" src="<?php echo esc_url($features_image['url']); ?>" alt="<?php echo esc_attr($features_image['alt']); ?>" >
+                <?php endif; ?>
             </div>
 
         </div>
