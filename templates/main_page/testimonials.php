800a6f63b3d1839c670449e77bfb6bba8c1393a9
gandomi <m.gandomi1995@gmail.com>
2023-10-28 13:08:56 +0330

    add testimonials acf template

--- templates/main_page/testimonials.php
+++ templates/main_page/testimonials.php
@@ -9,30 +9,17 @@
         <div class="video-carousel swiper mb-10">
             <!-- Additional required wrapper -->
             <div class="swiper-wrapper">
-                <!-- Slide -->
-                <div class="swiper-slide">
-                    <img class="w-100" src="images/main/video_carousel.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide">
-                    <img class="w-100" src="images/main/video_carousel.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide">
-                    <img class="w-100" src="images/main/video_carousel.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide">
-                    <img class="w-100" src="images/main/video_carousel.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide">
-                    <img class="w-100" src="images/main/video_carousel.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide">
-                    <img class="w-100" src="images/main/video_carousel.webp" alt="video poster for video carousel" role="button">
-                </div>
+
+                <?php if( have_rows('testimonials_content_video') ): ?>
+                    <?php while( have_rows('testimonials_content_video') ): the_row(); ?>
+                        <?php $image = get_sub_field('image'); ?>
+                        <?php if( !empty( $image ) ): ?>
+                            <div class="swiper-slide">
+                                <img role="button" class="w-100" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" >
+                            </div>
+                        <?php endif; ?>
+                    <?php endwhile; ?>
+                <?php endif; ?>
             </div>
         </div>
 
@@ -42,86 +29,17 @@
         <div class="customer-logo-carousel swiper mb-10">
             <!-- Additional required wrapper -->
             <div class="swiper-wrapper">
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_01.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_02.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide  w-auto">
-                    <img src="images/main/logo_03.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_04.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_05.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_06.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_07.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_08.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_09.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_10.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_01.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_02.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide  w-auto">
-                    <img src="images/main/logo_03.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_04.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_05.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_06.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_07.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_08.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_09.webp" alt="video poster for video carousel" role="button">
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide w-auto">
-                    <img src="images/main/logo_10.webp" alt="video poster for video carousel" role="button">
-                </div>
+                <?php if( have_rows('testimonials_content_logos') ): ?>
+                    <?php while( have_rows('testimonials_content_logos') ): the_row(); ?>
+                        <?php $image = get_sub_field('image'); ?>
+                        <?php if( !empty( $image ) ): ?>
+                            <!-- Slide -->
+                            <div class="swiper-slide w-auto">
+                                <img role="button" class="w-100" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" >
+                            </div>
+                        <?php endif; ?>
+                    <?php endwhile; ?>
+                <?php endif; ?>
             </div>
         </div>
 
@@ -132,96 +50,31 @@
         <div class="customer-testimonials-carousel swiper mb-10">
             <!-- Additional required wrapper -->
             <div class="swiper-wrapper">
-                <!-- Slide -->
-                <div class="swiper-slide">
-                    <div class="customer-testimonials-carousel__card bg-gray-800 rounded-3 p-6">
-                        <div class="mb-3 d-flex align-items-center justify-content-between">
-                            <div class="d-flex">
-                                <div class="ms-3 d-flex align-items-center">
-                                    <img src="images/main/customer_avatar.webp" alt="customer avatar">
+                <?php if( have_rows('testimonials_content_comments') ): ?>
+                    <?php while( have_rows('testimonials_content_comments') ): the_row(); ?>
+                        <!-- Slide -->
+                        <div class="swiper-slide">
+                            <div class="customer-testimonials-carousel__card bg-gray-800 rounded-3 p-6">
+                                <div class="mb-3 d-flex align-items-center justify-content-between">
+                                    <div class="d-flex">
+                                        <div class="ms-3 d-flex align-items-center">
+                                            <?php $customer_avatar = get_sub_field('customer_avatar'); ?>
+                                            <?php if( !empty( $customer_avatar ) ): ?>
+                                                <img src="<?php echo esc_url($customer_avatar['url']); ?>" alt="<?php echo esc_attr($customer_avatar['alt']); ?>" >
+                                            <?php endif; ?>
+                                        </div>
+                                        <div>
+                                            <h4 class="text-gray-50 display-5 fw-500 mb-1"><?php echo get_sub_field('title'); ?></h4>
+                                            <span class="font-pinar text-gray-300 fs-4"><?php echo get_sub_field('subtitle'); ?></span>
+                                        </div>
+                                    </div>
+                                    <span class="quote-mark text-gray-600">”</span>
                                 </div>
-                                <div>
-                                    <h4 class="text-gray-50 display-5 fw-500 mb-1">فریبا نادری اصیل</h4>
-                                    <span class="font-pinar text-gray-300 fs-4">مدیر عامل هتل عالی قاپو</span>
-                                </div>
+                                <p class="text-gray-400 mb-0"><?php echo get_sub_field('text'); ?></p>
                             </div>
-                            <span class="quote-mark text-gray-600">”</span>
                         </div>
-                        <p class="text-gray-400 mb-0">مقیاس‌پذیری زیرساخت‌های ابری کمک می‌کند تا به تناسب رشد یک کسب‌وکار، دغدغه‌های زیرساختی نداشته باشید.</p>
-                    </div>
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide">
-                    <div class="customer-testimonials-carousel__card bg-gray-800 rounded-3 p-6">
-                        <div class="mb-3 d-flex align-items-center justify-content-between">
-                            <div class="d-flex">
-                                <div class="ms-3 d-flex align-items-center">
-                                    <img src="images/main/customer_avatar.webp" alt="customer avatar">
-                                </div>
-                                <div>
-                                    <h4 class="text-gray-50 display-5 fw-500 mb-1">فریبا نادری اصیل</h4>
-                                    <span class="font-pinar text-gray-300 fs-4">مدیر عامل هتل عالی قاپو</span>
-                                </div>
-                            </div>
-                            <span class="quote-mark text-gray-600">”</span>
-                        </div>
-                        <p class="text-gray-400 mb-0">مقیاس‌پذیری زیرساخت‌های ابری کمک می‌کند تا به تناسب رشد یک کسب‌وکار، دغدغه‌های زیرساختی نداشته باشید.</p>
-                    </div>
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide">
-                    <div class="customer-testimonials-carousel__card bg-gray-800 rounded-3 p-6">
-                        <div class="mb-3 d-flex align-items-center justify-content-between">
-                            <div class="d-flex">
-                                <div class="ms-3 d-flex align-items-center">
-                                    <img src="images/main/customer_avatar.webp" alt="customer avatar">
-                                </div>
-                                <div>
-                                    <h4 class="text-gray-50 display-5 fw-500 mb-1">فریبا نادری اصیل</h4>
-                                    <span class="font-pinar text-gray-300 fs-4">مدیر عامل هتل عالی قاپو</span>
-                                </div>
-                            </div>
-                            <span class="quote-mark text-gray-600">”</span>
-                        </div>
-                        <p class="text-gray-400 mb-0">مقیاس‌پذیری زیرساخت‌های ابری کمک می‌کند تا به تناسب رشد یک کسب‌وکار، دغدغه‌های زیرساختی نداشته باشید.</p>
-                    </div>
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide">
-                    <div class="customer-testimonials-carousel__card bg-gray-800 rounded-3 p-6">
-                        <div class="mb-3 d-flex align-items-center justify-content-between">
-                            <div class="d-flex">
-                                <div class="ms-3 d-flex align-items-center">
-                                    <img src="images/main/customer_avatar.webp" alt="customer avatar">
-                                </div>
-                                <div>
-                                    <h4 class="text-gray-50 display-5 fw-500 mb-1">فریبا نادری اصیل</h4>
-                                    <span class="font-pinar text-gray-300 fs-4">مدیر عامل هتل عالی قاپو</span>
-                                </div>
-                            </div>
-                            <span class="quote-mark text-gray-600">”</span>
-                        </div>
-                        <p class="text-gray-400 mb-0">مقیاس‌پذیری زیرساخت‌های ابری کمک می‌کند تا به تناسب رشد یک کسب‌وکار، دغدغه‌های زیرساختی نداشته باشید.</p>
-                    </div>
-                </div>
-                <!-- Slide -->
-                <div class="swiper-slide">
-                    <div class="customer-testimonials-carousel__card bg-gray-800 rounded-3 p-6">
-                        <div class="mb-3 d-flex align-items-center justify-content-between">
-                            <div class="d-flex">
-                                <div class="ms-3 d-flex align-items-center">
-                                    <img src="images/main/customer_avatar.webp" alt="customer avatar">
-                                </div>
-                                <div>
-                                    <h4 class="text-gray-50 display-5 fw-500 mb-1">فریبا نادری اصیل</h4>
-                                    <span class="font-pinar text-gray-300 fs-4">مدیر عامل هتل عالی قاپو</span>
-                                </div>
-                            </div>
-                            <span class="quote-mark text-gray-600">”</span>
-                        </div>
-                        <p class="text-gray-400 mb-0">مقیاس‌پذیری زیرساخت‌های ابری کمک می‌کند تا به تناسب رشد یک کسب‌وکار، دغدغه‌های زیرساختی نداشته باشید.</p>
-                    </div>
-                </div>
+                    <?php endwhile; ?>
+                <?php endif; ?>
             </div>
         </div>
 
