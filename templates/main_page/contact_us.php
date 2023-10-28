84062ac79a735b8f53c29efc09b9326b3144c7f1
gandomi <m.gandomi1995@gmail.com>
2023-10-28 17:07:13 +0330

    add contact us main page section acf

--- templates/main_page/contact_us.php
+++ templates/main_page/contact_us.php
@@ -7,18 +7,14 @@
             <div class="col d-flex align-items-center">
 
                 <!-- Section Title Icon -->
-                <div class="section-title__icon ms-4">
-                    <span class="icon-support-bulk text-primary">
-                        <span class="path1"></span>
-                        <span class="path2"></span>
-                        <span class="path3"></span>
-                    </span>
+                <div class="section-title__icon ms-4 text-primary">
+                    <?php echo get_field('contact_us_icon'); ?>
                 </div>
 
                 <!-- Section Title Text -->
                 <div class="section-title__text">
-                    <p class="font-pinar fw-500 fs-2 mb-2 text-gray-200">پشتیبانی و مشاوره</p>
-                    <h2 class="display-4 fw-800 text-gray-100">لبخند رضایت‌تان را با دنیا عوض نمی‌کنیم</h2>
+                    <p class="font-pinar fw-500 fs-2 mb-2 text-gray-200"><?php echo get_field('contact_us_subtitle'); ?></p>
+                    <h2 class="display-4 fw-800 text-gray-100"><?php echo get_field('contact_us_title'); ?></h2>
                 </div>
             </div>
         </div>
@@ -29,35 +25,35 @@
             <div class="col-12 col-xl-7 mb-10 mb-xl-0">
                 <div class="row">
                     <div class="col-12 col-xl-6 main-contact-us__image text-xl-start">
-                        <img src="images/main/contact_us_01.webp" alt="contact us image female">
+
+                        <?php $contact_us_female_image = get_field('contact_us_female_image'); ?>
+                        <?php if( !empty( $contact_us_female_image ) ): ?>
+                            <img src="<?php echo esc_url($contact_us_female_image['url']); ?>" alt="<?php echo esc_attr($contact_us_female_image['alt']); ?>" >
+                        <?php endif; ?>
+
                         <div class="bg-gray-800 text-gray-100 d-inline-block main-contact-us__image__text main-contact-us__image__text--female text-end">
-                            <p class="m-0">خیالتون راحت دایرکت اچار فرانسه کسب و کار شماست.</p>
+                            <p class="m-0"><?php echo get_field('contact_us_female_image_text'); ?></p>
                         </div>
+
                     </div>
                     <div class="col-12 col-xl-6 main-contact-us__image mt-8 mt-xl-12">
-                        <img src="images/main/contact_us_02.webp" alt="contact us image male">
+
+                        <?php $contact_us_male_image = get_field('contact_us_male_image'); ?>
+                        <?php if( !empty( $contact_us_male_image ) ): ?>
+                            <img src="<?php echo esc_url($contact_us_male_image['url']); ?>" alt="<?php echo esc_attr($contact_us_female_image['alt']); ?>" >
+                        <?php endif; ?>
+
                         <div class="bg-gray-800 text-gray-100 d-inline-block main-contact-us__image__text main-contact-us__image__text--male">
-                            <p class="m-0">فروشم کم شده رقبا هر روز فروششون بیشتر میشه، چکار کنم؟</p>
+                            <p class="m-0"><?php echo get_field('contact_us_male_image_text'); ?></p>
                         </div>
+
                     </div>
                 </div>
             </div>
 
             <!-- Form -->
             <div class="col-12 col-xl-5">
-                <form class="main-contact-us__form me-auto" action="/">
-                    <div class="mb-5">
-                        <label for="name" class="form-label text-white-500">نام و نام خانوادگی</label>
-                        <input type="text" class="form-control bg-gray-800 border-0 py-5 px-4 rounded-3 mb-1 text-white-500" id="name" placeholder="نام و نام خانوادگی">
-                        <span class="text-gray-400 fs-4">این یک متن راهنمایی برای کمک به کاربر است.</span>
-                    </div>
-                    <div class="mb-5">
-                        <label for="phone-number" class="form-label text-white-500">شماره تماس</label>
-                        <input type="text" class="form-control bg-gray-800 border-0 py-5 px-4 rounded-3 mb-1 text-white-500 text-start" id="phone-number" placeholder="۰۹">
-                        <span class="text-gray-400 fs-4">این یک متن راهنمایی برای کمک به کاربر است.</span>
-                    </div>
-                    <button type="button" class="btn btn-primary w-100 text-white-500 p-4 rounded-3">درخواست پشتیبانی</button>
-                </form>
+                <?php echo do_shortcode(get_field('contact_us_contact_form_sortcode')); ?>
             </div>
         </div>
     </div>
