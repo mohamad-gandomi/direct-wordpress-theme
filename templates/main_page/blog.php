71fdf474805e3eea9432420b708254847684646a
gandomi <m.gandomi1995@gmail.com>
2023-10-28 14:03:09 +0330

    add blog section acf template

--- templates/main_page/blog.php
+++ templates/main_page/blog.php
@@ -7,18 +7,14 @@
             <div class="col d-flex align-items-center">
 
                 <!-- Section Title Icon -->
-                <div class="section-title__icon ms-4">
-                    <span class="icon-monitor-mobbile-bulk text-warning">
-                        <span class="path1"></span>
-                        <span class="path2"></span>
-                        <span class="path3"></span>
-                    </span>
+                <div class="section-title__icon ms-4 text-warning">
+                    <?php echo get_field('blog_icon'); ?>
                 </div>
 
                 <!-- Section Title Text -->
                 <div class="section-title__text">
-                    <p class="font-pinar fw-500 fs-2 mb-2 text-gray-200">یادگیری</p>
-                    <h2 class="display-4 fw-800 text-gray-100">مقالات</h2>
+                    <p class="font-pinar fw-500 fs-2 mb-2 text-gray-200"><?php echo get_field('blog_subtitel'); ?></p>
+                    <h2 class="display-4 fw-800 text-gray-100"><?php echo get_field('blog_title'); ?></h2>
                 </div>
             </div>
         </div>
@@ -26,167 +22,103 @@
         <!-- Blog Posts -->
         <div class="blog row">
 
-            <!-- Blog Post -->
-            <div class="col-12 mb-6 mb-xxl-0 col-xl-6 col-xxl-4">
-                <div class="blog__card rounded-6 bg-gray-800">
-                    <div class="blog__card__image">
-                        <img class="w-100" src="images/main/blog_feature_image.webp" alt="author profile image">
-                        <div class="blog__card__image__icons">
-                            <span class="icon-heart-bulk text-black-200 display-4 ms-3">
-                                <span class="path1"></span>
-                                <span class="path2"></span>
-                            </span>
-                            <span class="icon-archive-bulk text-black-200 display-4">
-                                <span class="path1"></span>
-                                <span class="path2"></span>
-                                <span class="path3"></span>
-                                <span class="path4"></span>
-                            </span>
-                        </div>
-                        <div class="blog__card__image__info d-flex align-items-center">
-                            <div class="blog__card__image__category p-3 text-secondary ms-3 d-flex align-items-center rounded-2">
-                                <span class="display-5 ms-3 icon-folder-open-line"></span>
-                                <span>باشگاه مشتریان</span>
-                            </div>
-                            <div class="blog__card__image__comments-no p-3 text-gray-50 ms-3 d-flex align-items-center rounded-2">
-                                <span class="display-5 ms-3 icon-message-text-line"></span>
-                                <span>12</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="blog__card__text p-6">
-                        <h3 class="font-yekanbakh text-gray-50 display-5 fw-600 mb-2">متداول ترین اشتباهات رایج در اجرای باشگاه مشتریان</h3>
-                        <p class="text-gray-200 fs-4 mb-6">امروزه به دلیل فضای رقابتی که در کسب‌وکارها وجود دارد، راه اندازی و اجرای باشگاه…</p>
-                        <div class="d-flex align-items-center justify-content-between">
-                            <div>
-                                <img class="ms-3" src="images/main/author_avatar.webp" alt="author profile image">
-                                <span class="text-white-500">امیر رضا قائمی</span>
-                            </div>
-                            <div class="blog__card__text__date d-flex align-items-center">
-                                <span class="icon-calendar-bulk text-gray-500 display-4 ms-3">
-                                    <span class="path1"></span>
-                                    <span class="path2"></span>
-                                    <span class="path3"></span>
-                                    <span class="path4"></span>
-                                    <span class="path5"></span>
-                                    <span class="path6"></span>
-                                    <span class="path7"></span>
-                                    <span class="path8"></span>
-                                </span>
-                                <span class="text-gray-500">30 شهریور 1402</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <?php
+            // Define the custom query to retrieve posts
+            $args = array(
+                'post_type' => 'post',  // You can specify the post type here (e.g., 'post', 'page', 'custom_post_type')
+                'posts_per_page' => -1, // To retrieve all posts, use -1, or specify the number of posts you want to display.
+            );
 
-            <!-- Blog Post -->
-            <div class="col-12 mb-6 mb-xxl-0 col-xl-6 col-xxl-4">
-                <div class="blog__card rounded-6 bg-gray-800">
-                    <div class="blog__card__image">
-                        <img class="w-100" src="images/main/blog_feature_image.webp" alt="author profile image">
-                        <div class="blog__card__image__icons">
-                            <span class="icon-heart-bulk text-black-200 display-4 ms-3">
-                                <span class="path1"></span>
-                                <span class="path2"></span>
-                            </span>
-                            <span class="icon-archive-bulk text-black-200 display-4">
-                                <span class="path1"></span>
-                                <span class="path2"></span>
-                                <span class="path3"></span>
-                                <span class="path4"></span>
-                            </span>
-                        </div>
-                        <div class="blog__card__image__info d-flex align-items-center">
-                            <div class="blog__card__image__category p-3 text-secondary ms-3 d-flex align-items-center rounded-2">
-                                <span class="display-5 ms-3 icon-folder-open-line"></span>
-                                <span>باشگاه مشتریان</span>
-                            </div>
-                            <div class="blog__card__image__comments-no p-3 text-gray-50 ms-3 d-flex align-items-center rounded-2">
-                                <span class="display-5 ms-3 icon-message-text-line"></span>
-                                <span>12</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="blog__card__text p-6">
-                        <h3 class="font-yekanbakh text-gray-50 display-5 fw-600 mb-2">متداول ترین اشتباهات رایج در اجرای باشگاه مشتریان</h3>
-                        <p class="text-gray-200 fs-4 mb-6">امروزه به دلیل فضای رقابتی که در کسب‌وکارها وجود دارد، راه اندازی و اجرای باشگاه…</p>
-                        <div class="d-flex align-items-center justify-content-between">
-                            <div>
-                                <img class="ms-3" src="images/main/author_avatar.webp" alt="author profile image">
-                                <span class="text-white-500">امیر رضا قائمی</span>
-                            </div>
-                            <div class="blog__card__text__date d-flex align-items-center">
-                                <span class="icon-calendar-bulk text-gray-500 display-4 ms-3">
-                                    <span class="path1"></span>
-                                    <span class="path2"></span>
-                                    <span class="path3"></span>
-                                    <span class="path4"></span>
-                                    <span class="path5"></span>
-                                    <span class="path6"></span>
-                                    <span class="path7"></span>
-                                    <span class="path8"></span>
-                                </span>
-                                <span class="text-gray-500">30 شهریور 1402</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            $custom_query = new WP_Query($args);
 
-            <!-- Blog Post -->
-            <div class="col-12 mb-6 mb-xxl-0 col-xl-6 col-xxl-4">
-                <div class="blog__card rounded-6 bg-gray-800">
-                    <div class="blog__card__image">
-                        <img class="w-100" src="images/main/blog_feature_image.webp" alt="author profile image">
-                        <div class="blog__card__image__icons">
-                            <span class="icon-heart-bulk text-black-200 display-4 ms-3">
-                                <span class="path1"></span>
-                                <span class="path2"></span>
-                            </span>
-                            <span class="icon-archive-bulk text-black-200 display-4">
-                                <span class="path1"></span>
-                                <span class="path2"></span>
-                                <span class="path3"></span>
-                                <span class="path4"></span>
-                            </span>
+            // Check if there are posts to display
+            if ($custom_query->have_posts()) :
+                while ($custom_query->have_posts()) : $custom_query->the_post();
+
+                    $post_title = get_the_title();
+                    $post_excerpt = get_the_excerpt();
+                    $post_author = get_the_author();
+                    $post_date = get_the_date();
+                    $post_categories = get_the_category();
+                    $post_thumbnail = (has_post_thumbnail()) ? get_the_post_thumbnail_url() : '';
+                    $post_comments_count = get_comments_number();
+                    $post_author_id = get_the_author_meta('ID');
+                    $post_author_avatar_url = get_avatar_url($post_author_id, array('size' => 32));
+
+                    ?>
+                        <!-- Blog Post -->
+                        <div class="col-12 mb-6 mb-xxl-0 col-xl-6 col-xxl-4">
+                            <div class="blog__card rounded-6 bg-gray-800">
+                                <div class="blog__card__image">
+                                    <img class="w-100" src="<?php echo $post_thumbnail; ?>" alt="<?php echo $post_title; ?>">
+                                    <div class="blog__card__image__icons">
+                                        <span class="icon-heart-bulk text-black-200 display-4 ms-3">
+                                            <span class="path1"></span>
+                                            <span class="path2"></span>
+                                        </span>
+                                        <span class="icon-archive-bulk text-black-200 display-4">
+                                            <span class="path1"></span>
+                                            <span class="path2"></span>
+                                            <span class="path3"></span>
+                                            <span class="path4"></span>
+                                        </span>
+                                    </div>
+                                    <div class="blog__card__image__info d-flex align-items-center">
+                                        <div class="blog__card__image__category p-3 text-secondary ms-3 d-flex align-items-center rounded-2">
+                                            <span class="display-5 ms-3 icon-folder-open-line"></span>
+                                            <span>
+                                                <?php
+                                                if (isset($post_categories) && !empty($post_categories)) {
+                                                    foreach ($post_categories as $category) {
+                                                        echo $category->name;
+                                                    }
+                                                }
+                                                ?>
+                                            </span>
+                                        </div>
+                                        <div class="blog__card__image__comments-no p-3 text-gray-50 ms-3 d-flex align-items-center rounded-2">
+                                            <span class="display-5 ms-3 icon-message-text-line"></span>
+                                            <span><?php echo $post_comments_count ?></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="blog__card__text p-6">
+                                    <h3 class="font-yekanbakh text-gray-50 display-5 fw-600 mb-2"><?php echo $post_title; ?></h3>
+                                    <p class="text-gray-200 fs-4 mb-6"><?php echo $post_excerpt; ?></p>
+                                    <div class="d-flex align-items-center justify-content-between">
+                                        <div>
+                                            <img class="ms-3 rounded-circle" src="<?php echo $post_author_avatar_url; ?>" alt="<?php echo $post_author; ?> profile image">
+                                            <span class="text-white-500"><?php echo $post_author; ?></span>
+                                        </div>
+                                        <div class="blog__card__text__date d-flex align-items-center">
+                                            <span class="icon-calendar-bulk text-gray-500 display-4 ms-3">
+                                                <span class="path1"></span>
+                                                <span class="path2"></span>
+                                                <span class="path3"></span>
+                                                <span class="path4"></span>
+                                                <span class="path5"></span>
+                                                <span class="path6"></span>
+                                                <span class="path7"></span>
+                                                <span class="path8"></span>
+                                            </span>
+                                            <span class="text-gray-500"><?php echo $post_date; ?></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="blog__card__image__info d-flex align-items-center">
-                            <div class="blog__card__image__category p-3 text-secondary ms-3 d-flex align-items-center rounded-2">
-                                <span class="display-5 ms-3 icon-folder-open-line"></span>
-                                <span>باشگاه مشتریان</span>
-                            </div>
-                            <div class="blog__card__image__comments-no p-3 text-gray-50 ms-3 d-flex align-items-center rounded-2">
-                                <span class="display-5 ms-3 icon-message-text-line"></span>
-                                <span>12</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="blog__card__text p-6">
-                        <h3 class="font-yekanbakh text-gray-50 display-5 fw-600 mb-2">متداول ترین اشتباهات رایج در اجرای باشگاه مشتریان</h3>
-                        <p class="text-gray-200 fs-4 mb-6">امروزه به دلیل فضای رقابتی که در کسب‌وکارها وجود دارد، راه اندازی و اجرای باشگاه…</p>
-                        <div class="d-flex align-items-center justify-content-between">
-                            <div>
-                                <img class="ms-3" src="images/main/author_avatar.webp" alt="author profile image">
-                                <span class="text-white-500">امیر رضا قائمی</span>
-                            </div>
-                            <div class="blog__card__text__date d-flex align-items-center">
-                                <span class="icon-calendar-bulk text-gray-500 display-4 ms-3">
-                                    <span class="path1"></span>
-                                    <span class="path2"></span>
-                                    <span class="path3"></span>
-                                    <span class="path4"></span>
-                                    <span class="path5"></span>
-                                    <span class="path6"></span>
-                                    <span class="path7"></span>
-                                    <span class="path8"></span>
-                                </span>
-                                <span class="text-gray-500">30 شهریور 1402</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                    <?php
+
+                endwhile;
+
+                // Restore the global post data
+                wp_reset_postdata();
+
+            else :
+                echo 'No posts found';
+            endif;
+        ?>
+
 
         </div>
     </div>
