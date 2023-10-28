c363fa99428cc2bf014e634264a0ef9e51aa4a8c
gandomi <m.gandomi1995@gmail.com>
2023-10-28 17:35:20 +0330

    add faq section acf template

--- templates/main_page/faqs.php
+++ templates/main_page/faqs.php
@@ -7,17 +7,14 @@
             <div class="col d-flex align-items-center">
 
                 <!-- Section Title Icon -->
-                <div class="section-title__icon ms-4">
-                    <span class="icon-messages-bulk text-success">
-                        <span class="path1"></span>
-                        <span class="path2"></span>
-                    </span>
+                <div class="section-title__icon ms-4 text-success">
+                    <?php echo get_field('faq_icon'); ?>
                 </div>
 
                 <!-- Section Title Text -->
                 <div class="section-title__text">
-                    <p class="font-pinar fw-500 fs-2 mb-2 text-gray-200">سوالات شما</p>
-                    <h2 class="display-4 fw-800 text-gray-100">سوالات متداول</h2>
+                    <p class="font-pinar fw-500 fs-2 mb-2 text-gray-200"><?php echo get_field('faq_subtitle'); ?></p>
+                    <h2 class="display-4 fw-800 text-gray-100"><?php echo get_field('faq_title'); ?></h2>
                 </div>
             </div>
         </div>
@@ -25,50 +22,21 @@
         <div class="row">
             <div class="col-12">
                 <div class="accordion accordion-flush px-0" id="accordionFlushExample">
-                    <!-- Faq Item -->
-                    <div class="accordion-item bg-black-500 mb-9">
-                        <h2 class="accordion-header border-0" id="flush-headingOne-header">
-                            <button class="accordion-button collapsed bg-black-500 text-gray-50 fs-2 fw-bold shadow-none pb-8 pt-0 px-0 text-end" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                                ۱- دستگاه دایرکت دقیقا چه کاری انجام می‌دهد؟
-                            </button>
-                        </h2>
-                        <div id="flush-collapseOne" class="accordion-collapse collapse text-light bg-black-500 fs-4 border-0" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body pb-6 pt-0 px-0">با استفاده از باشگاه مشتریان دایرکت شما می توانید شماره تماس مشتریان خود را به گونه ای محترمانه از آن‌ها دریافت کنید. پس از دریافت و ذخیره این شماره ها در پنل دایرکت می توانید با توجه به اهداف و استراتژی بازاریابی خود برای آن ها پیامک ارسال کنید.</div>
-                        </div>
-                    </div>
-                    <!-- Faq Item -->
-                    <div class="accordion-item bg-black-500 mb-9">
-                        <h2 class="accordion-header border-0" id="flush-headingTwo-header">
-                            <button class="accordion-button collapsed bg-black-500 text-gray-50 fs-2 fw-bold shadow-none pb-8 pt-0 px-0 text-end" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTow" aria-expanded="false" aria-controls="flush-collapseTow">
-                                ۲- دستگاه دایرکت دقیقا چه کاری انجام می‌دهد؟
-                            </button>
-                        </h2>
-                        <div id="flush-collapseTow" class="accordion-collapse collapse text-light bg-black-500 fs-4 border-0" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body pb-6 pt-0 px-0">با استفاده از باشگاه مشتریان دایرکت شما می توانید شماره تماس مشتریان خود را به گونه ای محترمانه از آن‌ها دریافت کنید. پس از دریافت و ذخیره این شماره ها در پنل دایرکت می توانید با توجه به اهداف و استراتژی بازاریابی خود برای آن ها پیامک ارسال کنید.</div>
-                        </div>
-                    </div>
-                    <!-- Faq Item -->
-                    <div class="accordion-item bg-black-500 mb-9">
-                        <h2 class="accordion-header border-0" id="flush-headingThree-header">
-                            <button class="accordion-button collapsed bg-black-500 text-gray-50 fs-2 fw-bold shadow-none pb-8 pt-0 px-0 text-end" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                                ۳- دستگاه دایرکت دقیقا چه کاری انجام می‌دهد؟
-                            </button>
-                        </h2>
-                        <div id="flush-collapseThree" class="accordion-collapse collapse text-light bg-black-500 fs-4 border-0" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body pb-6 pt-0 px-0">با استفاده از باشگاه مشتریان دایرکت شما می توانید شماره تماس مشتریان خود را به گونه ای محترمانه از آن‌ها دریافت کنید. پس از دریافت و ذخیره این شماره ها در پنل دایرکت می توانید با توجه به اهداف و استراتژی بازاریابی خود برای آن ها پیامک ارسال کنید.</div>
-                        </div>
-                    </div>
-                    <!-- Faq Item -->
-                    <div class="accordion-item bg-black-500 mb-9">
-                        <h2 class="accordion-header border-0" id="flush-headingFour-header">
-                            <button class="accordion-button collapsed bg-black-500 text-gray-50 fs-2 fw-bold shadow-none pb-8 pt-0 px-0 text-end" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
-                                ۴- دستگاه دایرکت دقیقا چه کاری انجام می‌دهد؟
-                            </button>
-                        </h2>
-                        <div id="flush-collapseFour" class="accordion-collapse collapse text-light bg-black-500 fs-4 border-0" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body pb-6 pt-0 px-0">با استفاده از باشگاه مشتریان دایرکت شما می توانید شماره تماس مشتریان خود را به گونه ای محترمانه از آن‌ها دریافت کنید. پس از دریافت و ذخیره این شماره ها در پنل دایرکت می توانید با توجه به اهداف و استراتژی بازاریابی خود برای آن ها پیامک ارسال کنید.</div>
-                        </div>
-                    </div>
+                    <?php if( have_rows('faq_content') ): $i = 0; ?>
+                        <?php while( have_rows('faq_content') ): the_row(); $i++; ?>
+                            <!-- Faq Item -->
+                            <div class="accordion-item bg-black-500 mb-9">
+                                <h2 class="accordion-header border-0" id="flush-headingOne-header">
+                                    <button class="accordion-button collapsed bg-black-500 text-gray-50 fs-2 fw-bold shadow-none pb-8 pt-0 px-0 text-end" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $i; ?>" aria-expanded="false" aria-controls="flush-collapse<?php echo $i; ?>">
+                                        <?php echo get_sub_field('question'); ?>
+                                    </button>
+                                </h2>
+                                <div id="flush-collapse<?php echo $i; ?>" class="accordion-collapse collapse text-light bg-black-500 fs-4 border-0" data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body pb-6 pt-0 px-0"><?php echo get_sub_field('answere'); ?></div>
+                                </div>
+                            </div>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
