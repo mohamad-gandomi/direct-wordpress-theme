e2100e9af8379aebafd8d08a4942ca2cc2842788
gandomi <m.gandomi1995@gmail.com>
2023-10-29 14:31:25 +0330

    add product information acf template

--- templates/product/product_information.php
+++ templates/product/product_information.php
@@ -2,58 +2,24 @@
     ================================================== -->
 <section class="product-information mb-14 mb-xl-16">
     <div class="container">
-
-        <div class="row align-items-center mb-12">
-            <div class="col-12 col-xl-6">
-                <div class="product-information__text">
-                    <h2 class="text-gray-50 display-4 fw-800 mb-5">کارت ویزیت دیجیتال</h2>
-                    <span class="font-pinar text-gray-200 d-block fw-500 mb-7">با دایرکت به استقبال مشتریان‌تان بروید</span>
-                    <p class="text-gray-200 d-block mb-7">با اولین خرید هر مشتری کارت ویزیت دیجیتال خود را که شامل پیامک خوشامدگویی، آدرس سایت یا شبکه‌های اجتماعی برای او ارسال کنید. این شروع یک ارتباط موثر برای فروش حداکثری شماست!</p>
+        <?php if( have_rows('product_information_content') ): $i = 0; ?>
+            <?php while( have_rows('product_information_content') ): the_row(); $i++; ?>
+                <div class="row <?php echo $i%2 == 0 ? 'flex-xl-row-reverse' : '' ?> align-items-center mb-12">
+                    <div class="col-12 col-xl-6">
+                        <div class="product-information__text <?php echo $i%2 == 0 ? 'me-xl-auto' : '' ?> ">
+                            <h2 class="text-gray-50 display-4 fw-800 mb-5"><?php echo get_sub_field('title'); ?></h2>
+                            <span class="font-pinar text-gray-200 d-block fw-500 mb-7"><?php echo get_sub_field('subtitle'); ?></span>
+                            <p class="text-gray-200 d-block mb-7"><?php echo get_sub_field('text'); ?></p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-xl-6">
+                        <?php $image = get_sub_field('image'); ?>
+                        <?php if( !empty( $image ) ): ?>
+                            <img class="w-100 product-information__img <?php echo $i%2 == 0 ? 'me-xl-auto' : '' ?> d-block" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" >
+                        <?php endif; ?>
+                    </div>
                 </div>
-            </div>
-            <div class="col-12 col-xl-6">
-                <img class="w-100 product-information__img me-xl-auto d-block" src="images/products/product_information_04.webp" alt="product information image">
-            </div>
-        </div>
-
-        <div class="row align-items-center flex-xl-row-reverse mb-12">
-            <div class="col-12 col-xl-6">
-                <div class="product-information__text me-xl-auto">
-                    <h2 class="text-gray-50 display-4 fw-800 mb-5">مدیریت حرفه‌ای مشتریان</h2>
-                    <span class="font-pinar text-gray-200 d-block fw-500 mb-7">با دایرکت مشتریان‌تان را به درستی هدف بگیرید</span>
-                    <p class="text-gray-200 d-block mb-7">با مدیریت حرفه‌ای مشتریان در دستگاه دایرکت می‌توانید برای هر گروه از مشتریان‌تان یک پیشنهاد ویژه داشته باشید و به هدفمند‌ترین حالت ممکن آن‌ها ترغیب به خرید کنید. یعنی به جای ارسال یک پیامک برای تمام مشتریان‌تان، چند پیامک را به چند گروه از مشتریان‌تان به طور هدفمند ارسال می‌کنید.</p>
-                </div>
-            </div>
-            <div class="col-12 col-xl-6">
-                <img class="w-100 product-information__img" src="images/products/product_information_03.webp" alt="product information image">
-            </div>
-        </div>
-
-        <div class="row align-items-center mb-12">
-            <div class="col-12 col-xl-6">
-                <div class="product-information__text">
-                    <h2 class="text-gray-50 display-4 fw-800 mb-5">ارسال پیامک از روی نقشه</h2>
-                    <span class="font-pinar text-gray-200 d-block fw-500 mb-7">تنها چند قدم با مشتریان‌تان فاصله دارید</span>
-                    <p class="text-gray-200 d-block mb-7">اگر صاحب کافی‌شاپ، سالن آرایشی و زیبایی، رستوران، املاک، فروشگاه، خدمات خودرو و … باشید قطعا می‌دانید بهترین مشتریان شما کسانی هستند که به محل کسب‌وکارتان نزدیک‌ هستند و می‌توانند در عرض چند دقیقه به فروشگاه شما مراجعه کنند. پس با داشتن یک بانک اطلاعاتی از شماره افرادی که نزدیک فروشگاه هستند و ارسال پیامک به آن‌ها به راحتی می‌توانید از افراد عادی یک مشتری همیشگی بسازید.</p>
-                </div>
-            </div>
-            <div class="col-12 col-xl-6">
-                <img class="w-100 product-information__img me-xl-auto d-block" src="images/products/product_information_02.webp" alt="product information image">
-            </div>
-        </div>
-
-        <div class="row align-items-center flex-xl-row-reverse">
-            <div class="col-12 col-xl-6">
-                <div class="product-information__text me-xl-auto">
-                    <h2 class="text-gray-50 display-4 fw-800 mb-5">تخفیف تولد </h2>
-                    <span class="font-pinar text-gray-200 d-block fw-500 mb-7">نشان دهید به یاد مشتریان‌تان هستید</span>
-                    <p class="text-gray-200 d-block mb-7">با ارسال پیامک تبریک تولد به صورت خودکار، می‌توانید به مشتریان‌تان نشان دهید که به یادشان هستید تا این حس مثبت و دلگرمی که از شما می‌گیرند باعث اعتماد بیشتر و ترغیب آن‌ها به خرید از شما شود.</p>
-                </div>
-            </div>
-            <div class="col-12 col-xl-6">
-                <img class="w-100 product-information__img" src="images/products/product_information_01.webp" alt="product information image">
-            </div>
-        </div>
-
+            <?php endwhile; ?>
+        <?php endif; ?>
     </div>
 </section>
